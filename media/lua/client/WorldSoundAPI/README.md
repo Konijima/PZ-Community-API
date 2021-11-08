5cde8f883caf1bb8686bf34cfe84997a02dbbb97
Konijima <mathieugermain1991@gmail.com>
2021-11-08 02:49:05 -0500

    WorldSoundAPI: Added code

--- media/lua/client/WorldSoundAPI/README.md
+++ media/lua/client/WorldSoundAPI/README.md
@@ -3,10 +3,30 @@
 **Contributors:**  -
 
 ## Description
-...
+Easily add Sounds at specific position categorized by unique name.
 
 ## How to use
-...
+```lua
+require("CommunityAPI")
+
+local worldSoundAPI = CommunityAPI.Client.WorldSound
+
+worldSoundAPI.AddSoundAt("computer_ambiant", x, y, z, {"ComputerBoot", "ComputerHum"})
+worldSoundAPI.AddSoundAt("computer_ambiant", x, y, z, "ComputerShutdown")
+worldSoundAPI.RemoveSoundAt("computer_ambiant", x, y, z)
+worldSoundAPI.RemoveAllSoundAt(x, y, z)
+```
 
 ## Methods
-...
+```lua
+AddSoundAt(name, x, y, z, soundList)
+--- soundList can be a string or a list of string to chain multiple sounds
+```
+
+```lua
+RemoveSoundAt(name, x, y, z)
+```
+
+```lua
+RemoveAllSoundAt(x, y, z)
+```
--- media/lua/client/WorldSoundAPI/WorldSoundAPIClient.lua
+++ media/lua/client/WorldSoundAPI/WorldSoundAPIClient.lua
@@ -1,4 +1,136 @@
+require("ISBaseObject")
+
+local DEBUG = getCore():getDebug()
+
+---@class WorldSoundObject
+local WorldSoundObject = ISBaseObject:derive("WorldSoundObject")
+
+function WorldSoundObject:isPlaying()
+    return self.audio ~= nil and self.soundEmitter:isPlaying(self.audio)
+end
+
+function WorldSoundObject:update()
+    if self.completed then return false; end
+
+    self.soundEmitter:tick()
+
+    if self.audio == nil or not self.soundEmitter:isPlaying(self.audio) then
+        self.index = self.index + 1
+        if self.soundList[self.index] then
+            self.audio = self.soundEmitter:playSound(self.soundList[self.index])
+        else
+            self.completed = true
+            self.soundEmitter = nil
+            return false
+        end
+    end
+    return true
+end
+
+function WorldSoundObject:destroy()
+    if self.soundEmitter then
+        self.soundEmitter:stopAll()
+        self.soundEmitter = nil
+
+    end
+end
+
+function WorldSoundObject:new(name, x ,y ,z, soundList)
+    local o = ISBaseObject:new()
+    setmetatable(o, self)
+    self.__index = self
+
+    if type(soundList) == "table" then
+        o.soundList = soundList
+    elseif type(soundList) == "string" then
+        o.soundList = {soundList}
+    else
+        error("WorldSoundAPI: Parameter soundList must be a string or a table of string.")
+    end
+
+    o.name = name
+    o.position = { x=x, y=y, z=z }
+
+    o.index = 0
+    o.audio = nil
+    o.completed = false
+    o.soundEmitter = fmod.fmod.FMODSoundEmitter.new()
+    o.soundEmitter:setPos(x, y, z)
+
+    return o
+end
+
+-----------------------------------------------------------
+
 local WorldSoundAPI = {}
 
+---@type table<string,table>
+local Sounds = {}
+
+local function positionToId(x, y, z)
+    return tostring(x) .. "|" .. tostring(y) .. "|" .. tostring(z)
+end
+
+---@param name string
+---@param x number
+---@param y number
+---@param z number
+---@param soundList string|table<string>
+function WorldSoundAPI.AddSoundAt(name, x, y, z, soundList)
+    local id = positionToId(x, y, z)
+    if not Sounds[id] then
+        Sounds[id] = {}
+    end
+    if Sounds[id] and Sounds[id][name] then
+        Sounds[id][name]:destroy()
+        Sounds[id][name] = nil
+    end
+
+    Sounds[id][name] = WorldSoundObject:new(name, x ,y, z, soundList)
+    if DEBUG then print("World sound ['"..name.."'] added at x:"..x.." y:"..y.." z:"..z) end
+end
+
+---@param name string
+---@param x number
+---@param y number
+---@param z number
+function WorldSoundAPI.RemoveSoundAt(name, x, y, z)
+    local id = positionToId(x, y, z)
+    if Sounds[id] and Sounds[id][name] then
+        Sounds[id][name]:destroy()
+        Sounds[id][name] = nil
+        if DEBUG then print("World sound ['"..name.."'] removed at x:"..x.." y:"..y.." z:"..z) end
+    end
+end
+
+---@param x number
+---@param y number
+---@param z number
+function WorldSoundAPI.RemoveAllSoundAt(x, y, z)
+    local id = positionToId(x, y, z)
+    if Sounds[id] then
+        for name in pairs(Sounds[id]) do
+            Sounds[id][name]:destroy()
+            Sounds[id][name] = nil
+            if DEBUG then print("World sound ['"..name.."'] removed at x:"..x.." y:"..y.." z:"..z) end
+        end
+        Sounds[id] = {}
+    end
+end
+
+local function Tick()
+    for id in pairs(Sounds) do
+        for name in pairs(Sounds[id]) do
+            if not Sounds[id][name].completed then
+                Sounds[id][name]:update()
+            else
+                local position = Sounds[id][name].position
+                Sounds[id][name] = nil
+                if DEBUG then print("World sound ['"..name.."'] completed at x:"..position.x.." y:"..position.y.." z:"..position.z) end
+            end
+        end
+    end
+end
+Events.OnTick.Add(Tick)
 
 return WorldSoundAPI
