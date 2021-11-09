0fdb40e2ce8ea9ae4bd7b79a8169099be828085d
Konijima <mathieugermain1991@gmail.com>
2021-11-08 22:51:42 -0500

    Added NumberToDecimalString()

--- Contents/mods/CommunityAPI/media/lua/shared/CommunityAPI/StringUtils.lua
+++ Contents/mods/CommunityAPI/media/lua/shared/CommunityAPI/StringUtils.lua
@@ -3,7 +3,7 @@ local StringUtils = {}
 --- Transform a square position into a unique string
 ---@param square IsoGridSquare
 ---@return string
-function StringUtils.squareToId(square)
+function StringUtils.SquareToId(square)
     if instanceof(square, "IsoGridSquare") then
         return square:getX() .. "|" .. square:getY() .. "|" .. square:getZ()
     end
@@ -14,7 +14,7 @@ end
 ---@param y number
 ---@param z number
 ---@return string
-function StringUtils.positionToId(x, y ,z)
+function StringUtils.PositionToId(x, y ,z)
     return tostring(x) .. "|" .. tostring(y) .. "|" .. tostring(z)
 end
 
@@ -22,12 +22,21 @@ end
 ---@param str string the string to split
 ---@param delimiter string the string to split with
 ---@return table<string>
-function StringUtils.splitString(str, delimiter)
+function StringUtils.SplitString(str, delimiter)
     local result = {}
     for match in (str..delimiter):gmatch("(.-)"..delimiter) do
         table.insert(result, match)
     end
     return result
+end
+
+--- Format a number into string with decimal
+---@param value number The number value to format
+---@param _decimal number Amount of decimal
+---@return string
+function StringUtils.NumberToDecimalString(value, _decimal)
+    if not type(_decimal) == "number" then _decimal = 2 end
+    return string.format("%.".._decimal.."f", value);
 end
 
 return StringUtils
