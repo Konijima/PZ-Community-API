e20e76283b6d98bf131a8d24ea8bd7298c25e82e
Aiteron <sashablinov1998@gmail.com>
2021-11-12 05:50:02 +0500

    Added Expanded Mod Selector

---
+++ Contents/mods/ExpandedModSelector/media/lua/client/ExpandedModSelector/ModPosterPanel.lua
@@ -0,0 +1,31 @@
+ModPosterPanel = ISPanelJoypad:derive("ModPosterPanel")
+
+function ModPosterPanel:render()
+	ISPanelJoypad.render(self)
+
+	if self.modInfo and (self.modInfo:getPosterCount() > 0) then
+		local index = self.parent.thumbnailPanel.index
+		index = math.max(index, 1)
+		index = math.min(index, self.modInfo:getPosterCount())
+		local texName = self.modInfo:getPoster(index - 1)
+		local tex = texName and getTexture(texName) or Texture.getWhite()
+		local left = 1
+		local top = 1
+		local alpha = 1.0
+		if tex == Texture.getWhite() then alpha = 0.1 end
+		local scrollBarWid = (self:getScrollHeight() > self.height) and self.vscroll:getWidth() or 0
+		self:drawTextureScaledAspect(tex, left, top, self.width - scrollBarWid - left, self.height - 1 - top, alpha, 1, 1, 1)
+	end
+end
+
+function ModPosterPanel:setJoypadFocused(focused)
+end
+
+function ModPosterPanel:setModInfo(modInfo)
+	self.modInfo = modInfo
+end
+
+function ModPosterPanel:new(x, y, width, height)
+	local o = ISPanelJoypad.new(self, x, y, width, height)
+	return o
+end
---
+++ Contents/mods/ExpandedModSelector/media/lua/client/ExpandedModSelector/ModThumbnailPanel.lua
@@ -0,0 +1,92 @@
+
+ModThumbnailPanel = ISPanelJoypad:derive("ModThumbnailPanel")
+
+function ModThumbnailPanel:render()
+	ISPanelJoypad.render(self)
+
+	if self.modInfo and (self.modInfo:getPosterCount() > 0) then
+		self.index = math.max(self.index, 1)
+		self.index = math.min(self.index, self.modInfo:getPosterCount())
+		local left = 1
+		local top = 1
+		local alpha = 1.0
+
+		local width = self.thumbnailWidth
+		local height = self.thumbnailHeight
+		local x = 0
+		local y = 0
+		for i=1,self.modInfo:getPosterCount() do
+			local tex = getTexture(self.modInfo:getPoster(i-1)) or Texture:getWhite()
+			if tex == Texture.getWhite() then alpha = 0.1 end
+			self:drawRect(x + self.padX, y + self.padY, width, height, 1, 0.1, 0.1, 0.1)
+			self:drawTextureScaledAspect(tex, x + self.padX, y + (self.height - height) / 2, width, height, alpha, 1, 1, 1)
+
+			if not self.pressed and self:isMouseOver() and self:getIndexAt(self:getMouseX(), self:getMouseY()) + 1 == i then
+				self:drawRectBorder(x + self.padX - 2, y + self.padY - 2, width + 4, height + 4, 1, 0.5, 0.5, 0.5)
+			end
+			
+			x = x + self.padX + width
+		end
+
+		if x > self.width then
+			self:setXScroll(math.min(self:getXScroll(), 0))
+			self:setXScroll(math.max(self:getXScroll(), -(x - self.width)))
+		else
+			self:setXScroll(0)
+		end
+	end
+end
+
+function ModThumbnailPanel:onMouseDown(x, y)
+	self.index = self:getIndexAt(x, y) + 1
+	self.pressed = true
+end
+
+function ModThumbnailPanel:onMouseUp(x, y)
+	self.pressed = false
+end
+
+function ModThumbnailPanel:onMouseUpOutside(x, y)
+	self.pressed = false
+end
+
+function ModThumbnailPanel:onMouseMove(dx, dy)
+	if self.pressed then
+		self:setXScroll(self:getXScroll() + dx)
+	end
+end
+
+function ModThumbnailPanel:onMouseMoveOutside(dx, dy)
+	if self.pressed then
+		self:onMouseMove(dx, dy)
+	end
+end
+
+function ModThumbnailPanel:getIndexAt(x, y)
+	if not self.modInfo or self.modInfo:getPosterCount() == 0 then
+		return -1
+	end
+	local index = (x - 5) / (self.thumbnailWidth + 10)
+	index = math.floor(index)
+	if index >= self.modInfo:getPosterCount() then
+		return -1
+	end
+	return index
+end
+
+function ModThumbnailPanel:setJoypadFocused(focused)
+end
+
+function ModThumbnailPanel:setModInfo(modInfo)
+	self.modInfo = modInfo
+end
+
+function ModThumbnailPanel:new(x, y, width, height)
+	local o = ISPanelJoypad.new(self, x, y, width, height)
+	o.padX = 10
+	o.padY = 8
+	o.thumbnailWidth = 116
+	o.thumbnailHeight = height - o.padY * 2 -- 64
+	o.index = 1
+	return o
+end
