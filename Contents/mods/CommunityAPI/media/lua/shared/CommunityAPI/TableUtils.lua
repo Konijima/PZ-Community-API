5f3b4e7dbd64b5efbd7abe83f8dfbcd77b4ac4e5
Konijima <mathieugermain1991@gmail.com>
2021-11-13 01:54:44 -0500

    Utils: Refactor Table param names and fix annotations

--- Contents/mods/CommunityAPI/media/lua/shared/CommunityAPI/TableUtils.lua
+++ Contents/mods/CommunityAPI/media/lua/shared/CommunityAPI/TableUtils.lua
@@ -1,7 +1,8 @@
 local TableUtils = {}
 
 --- Get the total count of entry in a table
----@param targetTable table The table to get count from
+---@param targetTable table The table to count total of entries
+---@return number
 function TableUtils.CountTableEntries(targetTable)
     local count = 0
     for _ in pairs(targetTable) do
@@ -11,7 +12,8 @@ function TableUtils.CountTableEntries(targetTable)
 end
 
 --- Get all the keys of a lua table
----@param targetTable table The table to get keys from
+---@param targetTable table The table to get the keys from
+---@return table<string>
 function TableUtils.GetTableKeys(targetTable)
     local keys = {}
     for key in pairs(targetTable) do
@@ -21,18 +23,18 @@ function TableUtils.GetTableKeys(targetTable)
 end
 
 --- Check if a value is found in a table
----@param table table The table to search in
----@param value any The value to find
+---@param targetTable table The table to search in
+---@param targetValue any The value to find
 ---@return boolean
-function TableUtils.TableContains(table, value)
-    if type(table) == "table" then
-        for i=1, #table do
-            if table[i] == value then
+function TableUtils.TableContains(targetTable, targetValue)
+    if type(targetTable) == "table" then
+        for i=1, #targetTable do
+            if targetTable[i] == targetValue then
                 return true
             end
         end
-        for _, v in pairs(table) do
-            if v == value then
+        for _, v in pairs(targetTable) do
+            if v == targetValue then
                 return true
             end
         end
@@ -40,14 +42,15 @@ function TableUtils.TableContains(table, value)
     return false
 end
 
---- Get the base class of object, optionally choose how deep you want to check.
----@param object table Will return nil if the object is not a table.
----@param _level  number Will return the deepest found if level is higher than the actual amount of base classes.
-function TableUtils.GetBaseClass(object, _level)
+--- Get the base class of an object, optionally choose how deep you want to check
+---@param tableObject table The table object to get the base class from
+---@param _level number Get the deepest base class found if level is higher than the total of base classes
+---@return table|nil
+function TableUtils.GetBaseClass(tableObject, _level)
     if not _level or _level < 1 then _level = 1; end
 
-    if type(object) == "table" then
-        local baseClass = getmetatable(object)
+    if type(tableObject) == "table" then
+        local baseClass = getmetatable(tableObject)
         for i=2, _level do
             if type(baseClass) == "table" then
                 local class = getmetatable(baseClass)
@@ -60,18 +63,18 @@ function TableUtils.GetBaseClass(object, _level)
     end
 end
 
---- Get a table containing all the base class from the current to the deepest.
----@param object table Will return nil if the object is not a table.
----@param _excludeCurrent boolean optionally exclude the current object class from the list
----@return table|nil
-function TableUtils.GetAllBaseClasses(object, _excludeCurrent)
-    if type(object) == "table" then
+--- Get list of all derived class from the current to the deepest level
+---@param tableObject table The table object to get all base class from
+---@param _excludeCurrent boolean Optionally exclude the current object class from the list
+---@return table<table>|nil
+function TableUtils.GetAllBaseClasses(tableObject, _excludeCurrent)
+    if type(tableObject) == "table" then
         local baseClasses = {}
-        local current = getmetatable(object)
+        local current = getmetatable(tableObject)
 
         local lastBaseClass
         for i=1, 10 do
-            local baseClass = TableUtils.GetBaseClass(object, i)
+            local baseClass = TableUtils.GetBaseClass(tableObject, i)
             if baseClass ~= nil and lastBaseClass ~= baseClass then
                 if not _excludeCurrent or _excludeCurrent and current ~= baseClass then
                     table.insert(baseClasses, baseClass)
@@ -87,15 +90,15 @@ function TableUtils.GetAllBaseClasses(object, _excludeCurrent)
 end
 
 --- Check if table object derive from this class
----@param object table The table object to check
----@param class table|string The class to find
+---@param tableObject table The table object to check
+---@param tableClass table|string The class to compare with
 ---@return boolean
-function TableUtils.IsClassChildOf(object, class)
-    local classType = type(class)
-    local allBaseClasses = TableUtils.GetAllBaseClasses(object, false)
+function TableUtils.IsClassChildOf(tableObject, tableClass)
+    local classType = type(tableClass)
+    local allBaseClasses = TableUtils.GetAllBaseClasses(tableObject, false)
     if allBaseClasses then
         for i=1, #allBaseClasses do
-            if (classType == "table" and allBaseClasses[i] == class) or (classType == "string" and allBaseClasses[i].Type == class) then
+            if (classType == "table" and allBaseClasses[i] == tableClass) or (classType == "string" and allBaseClasses[i].Type == tableClass) then
                 return true
             end
         end
