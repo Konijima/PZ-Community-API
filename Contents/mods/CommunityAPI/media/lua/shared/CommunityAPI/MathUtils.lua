ceca09f7e71630920acd0400a231fbcd43684c0e
Konijima <mathieugermain1991@gmail.com>
2021-11-13 01:24:34 -0500

    Utils: Refactor annotations

--- Contents/mods/CommunityAPI/media/lua/shared/CommunityAPI/MathUtils.lua
+++ Contents/mods/CommunityAPI/media/lua/shared/CommunityAPI/MathUtils.lua
@@ -1,13 +1,9 @@
 local MathUtils = {}
 
---- Get the distance between two point
 --- Get the 2D distance between two point
 ---@param x1 number X coordinate of first point
----@param y1 number y coordinate of first point
 ---@param y1 number Y coordinate of first point
 ---@param x2 number X coordinate of second point
----@param y2 number y coordinate of second point
-function MathUtils.GetDistance(x1, y1, x2, y2)
 ---@param y2 number Y coordinate of second point
 ---@return number
 function MathUtils.GetDistance2DBetweenPoints(x1, y1, x2, y2)
