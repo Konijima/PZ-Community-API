bff91a0d8fa178bd38c40d985aa6bdb8d659619c
Konijima <mathieugermain1991@gmail.com>
2021-11-13 01:23:53 -0500

    Utils: Refactor GetDistance to GetDistance2DBetweenPoints

--- Contents/mods/CommunityAPI/media/lua/shared/CommunityAPI/MathUtils.lua
+++ Contents/mods/CommunityAPI/media/lua/shared/CommunityAPI/MathUtils.lua
@@ -1,11 +1,16 @@
 local MathUtils = {}
 
 --- Get the distance between two point
+--- Get the 2D distance between two point
 ---@param x1 number X coordinate of first point
 ---@param y1 number y coordinate of first point
+---@param y1 number Y coordinate of first point
 ---@param x2 number X coordinate of second point
 ---@param y2 number y coordinate of second point
 function MathUtils.GetDistance(x1, y1, x2, y2)
+---@param y2 number Y coordinate of second point
+---@return number
+function MathUtils.GetDistance2DBetweenPoints(x1, y1, x2, y2)
     local a = x1 - x2
     local b = y1 - y2
     return math.sqrt( a*a + b*b )
