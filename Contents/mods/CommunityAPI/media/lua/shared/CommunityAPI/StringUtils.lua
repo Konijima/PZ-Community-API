e0cdcf3d3fd1b650e41d25ca8635b0c907d0f68e
Aiteron <sashablinov1998@gmail.com>
2021-11-15 00:12:45 +0500

    StringUtils: Fix EmmyLua param error

--- Contents/mods/CommunityAPI/media/lua/shared/CommunityAPI/StringUtils.lua
+++ Contents/mods/CommunityAPI/media/lua/shared/CommunityAPI/StringUtils.lua
@@ -19,7 +19,7 @@ end
 --- Split a string by a delimiter string
 ---@param str string The string to split
 ---@param delimiter string The string to split with
----@return table<string>
+---@return table<number, string>
 function StringUtils.SplitString(str, delimiter)
     local result = {}
     for match in (str..delimiter):gmatch("(.-)%"..delimiter) do
